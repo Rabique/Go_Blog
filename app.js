a719c1212ab9e5bb4e541d3657ae497d1fa89019
rabique <oddyseibcm@gmail.com>
2024-09-12 14:07:23 +0900

    1st commit

---
+++ app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const connectDb = require("./config/db");
+const cookieParser = require("cookie-parser");
+const methodOverride = require("method-override");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+connectDb();
+
+//레이아웃과 뷰 엔진 설정
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.static("public"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use(cookieParser());
+app.use(methodOverride("_method"));
+
+app.use("/", require("./routes/main"));
+app.use("/", require("./routes/admin"));
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
